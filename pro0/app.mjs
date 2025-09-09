389cc52b548acb57ebc09e4aed68ee911825cc3c
Shrey K <srkkadia@gmail.com>
2025-09-09 09:32:18 +0530

    commonjs,es

---
+++ pro0/app.mjs
@@ -0,0 +1,4 @@
+// const math = require('./math'); //CommonJS - import
+import { add, subtract } from './math.mjs'; //ES - import
+
+console.log(add(2, 3));
---
+++ pro0/math.mjs
@@ -0,0 +1,23 @@
+// CommonJS Module
+// function add(a, b) {
+//     return a+b;
+// }
+
+// function subtract(a, b) {
+//     return a-b;
+// }
+
+// module.exports = {  //CommomJS export
+//     add,
+//     subtract
+// };
+
+
+// ES Module
+export function add(a, b) {
+    return a+b;
+}
+
+export function subtract(a, b) {
+    return a-b;
+}
