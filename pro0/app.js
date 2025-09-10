ceb7975e915a3b17f0cf70371fc50ad0272ae94e
Shrey K <srkkadia@gmail.com>
2025-09-10 09:38:03 +0530

    schema

--- pro0/app.js
+++ pro0/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 3000
+
 const dbConnection = require('./util/db')
 dbConnection();
+
+const userModel = require('./models/UserModel')
+
+app.get('/users', async(req, res) => {
+    const users = await userModel.find()
+
+    res.status(200).json({
+        message: "Users list",
+        data: users,
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Express server running at http://127.0.0.1:${PORT}`);
--- pro0/util/db.js
+++ pro0/util/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 // mongodb://127.0.0.1:27017/<dbname>
-const uri = "mongodb://127.0.0.1:27017/node"
+const uri = ""
 
 const dbConnection = async() => {
     const connection = await mongoose.connect(uri)
