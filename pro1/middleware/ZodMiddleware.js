737d495bd9d8feee9d839c322c5ffc83b6179ceb
Shrey K <srkkadia@gmail.com>
2025-09-14 10:00:37 +0530

    zod Validation

---
+++ pro1/middleware/ZodMiddleware.js
@@ -0,0 +1,17 @@
+const validationSchema = (schema) => async (req, res, next) => {
+    try {
+        await schema.parseAsync({
+            body: req.body,
+            query: req.query,
+            params: req.params
+        });
+
+        next();
+    } catch (error) {
+        res.status(400).json({ message: error })
+    }
+}
+
+module.exports = {
+    validationSchema
+}
--- pro1/routes/UserRoutes.js
+++ pro1/routes/UserRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/UserController');
+const zodMiddleware = require('../middleware/ZodMiddleware');
+const userValidationSchema = require('../util/UserValidationSchema');
 
 router.get("/", userController.getAllUsers);
 router.get("/id/:id", userController.getUserById);
 router.get("/age/:age", userController.getUserByAge);
 router.get("/age-range", userController.getUserByAgeRange);
-router.post("/", userController.addUser);
+// router.post("/", userController.addUser);
+router.post("/", zodMiddleware.validationSchema(userValidationSchema), userController.addUser);
 router.put("/id/:id", userController.updateUserById);
 router.put("/email/:email", userController.updateUserByEmail);
 router.delete("/:id", userController.deleteUser);
