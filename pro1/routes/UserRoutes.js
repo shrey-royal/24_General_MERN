f3c54e92250ab8a5a388f3d09493c8d122fa52d8
Shrey K <srkkadia@gmail.com>
2025-09-14 10:12:35 +0530

    Auth

--- pro1/routes/UserRoutes.js
+++ pro1/routes/UserRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/UserController');
 const zodMiddleware = require('../middleware/ZodMiddleware');
+const authMiddleware = require('../middleware/AuthMiddleware');
 const userValidationSchema = require('../util/UserValidationSchema');
 
-router.get("/", userController.getAllUsers);
+router.get("/", authMiddleware.authMiddleware, userController.getAllUsers);
 router.get("/id/:id", userController.getUserById);
 router.get("/age/:age", userController.getUserByAge);
 router.get("/age-range", userController.getUserByAgeRange);
-// router.post("/", userController.addUser);
 router.post("/", zodMiddleware.validationSchema(userValidationSchema), userController.addUser);
 router.put("/id/:id", userController.updateUserById);
 router.put("/email/:email", userController.updateUserByEmail);
