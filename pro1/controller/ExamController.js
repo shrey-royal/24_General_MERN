16ca2c31d6c08ae8bd5948508e79171a58db5f56
Shrey K <srkkadia@gmail.com>
2025-09-15 09:29:48 +0530

    half exam, question

---
+++ pro1/controller/ExamController.js
@@ -0,0 +1,14 @@
+const examSchema = require("../model/ExamModel");
+
+const createExam = async (req, res) => {
+    //
+}
+
+const getAllExams = async (req, res) => {
+    //
+}
+
+module.exports = {
+    createExam,
+    getAllExams,
+}
---
+++ pro1/controller/QuestionController.js
@@ -0,0 +1,9 @@
+const questionModel = require("../model/QuestionModel")
+
+const createQuestion = async (req, res) => {
+    //
+}
+
+module.exports = {
+    createQuestion,
+}
